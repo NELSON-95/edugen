5cc232b5dfffae7a1ef9d72ac234116c578f096c
CHIKODILI <nelsonchikodili@gmail.com>
2023-05-04 05:18:39 -0700

    edugentech

---
+++ script.js
@@ -0,0 +1,17 @@
+// let hamburger= document.querySelector('.hamburger');
+// let item1= document.querySelector('.item1');
+// let item2= document.querySelector('.item2');
+// let item3= document.querySelector('.item3');
+// let navMenu= document.querySelector('.navMenu');
+
+
+// hamburger.addEventListener('click', ()=>{
+//     item1.classList.toggle('item11');
+//     item2.classList.toggle('item22');
+//     item3.classList.toggle('item33');
+//     navMenu.style.left='0px';
+// });
+
+console.log("javascript is the best");
+console.log("javascript is the best");
+console.log("javascript is the best");
