a9f3eee79805cf838ae1dec03a9591a3964a749f
CHIKODILI <nelsonchikodili@gmail.com>
2023-05-23 01:35:13 -0700

    first commit

--- script.js
+++ script.js
@@ -31,6 +31,9 @@ function changeNavColor(){
     }
 
 
+
+
+// project section slides script
 let imgBox1= document.querySelector(".imgBox1");
 let imgBox2= document.querySelector(".imgBox2");
 let imgBox3= document.querySelector(".imgBox3");
@@ -41,61 +44,78 @@ let imgBoxArray=[
     'img/hero3.jpg', 
     'img/hero4.jpg',
     'img/hero5.jpg', 
-    'img/hero6.jpg', 
-    // 'img/hero7.jpg',  
+    'img/hero6.jpg',  
 ];
-let imgBox1Arr= imgBoxArray.slice(0,2);
-let imgBox2Arr= imgBoxArray.slice(2,4);
-let imgBox3Arr= imgBoxArray.slice(4,6);
+let imgBox1Arr= imgBoxArray.slice(0,10);
+let imgBox2Arr= imgBoxArray.slice(10,20);
+let imgBox3Arr= imgBoxArray.slice(20,30);
+let dot =document.querySelectorAll('.dot');
 let time =0;
-
 let nextBtn= document.querySelector(".nextBtn");
 let prevBtn= document.querySelector(".prevBtn");
-
 nextBtn.addEventListener('click',slideImgNext);
 prevBtn.addEventListener('click',slideImgPrev);
 
+window.addEventListener('DOMContentLoaded', ()=>{
+     setTimeout(slideImgPrev,time)
+});
 
- window.addEventListener('DOMContentLoaded', ()=>{
-     setTimeout(slideImgNext,time)
- });
+// setInterval(slideImgNext,20000);
+// clearInterval(slideImgNext)
 
-function slideImgNext(){
-    
-    if(i<imgBox1Arr.length-1){
+function slideImgNext(){  
+    if(i<imgBox1Arr.length-1 ){
+
+        i++;
+        dot[i].style.backgroundColor="var(--green)";
         imgBox1.src=imgBox1Arr[i];
         imgBox2.src=imgBox2Arr[i];
         imgBox3.src=imgBox3Arr[i];
-        i++
-        prevBtn.style.opacity="1"; 
+        prevBtn.style.opacity="1";
         
     }
-    else{
-        nextBtn.style.opacity="0.5"
-        prevBtn.style.opacity="1"
-       
+    else {
+        i=imgBox1Arr.length-1;
+        dot[i].style.backgroundColor="var(--green)";
+        imgBox1.src=imgBox1Arr[i];
+        imgBox2.src=imgBox2Arr[i];
+        imgBox3.src=imgBox3Arr[i];
+        nextBtn.style.opacity="0.5";
     }
     
-}
-function slideImgPrev(){
     
+}
+function slideImgPrev(){ 
     if(i>0){
+        dot[i].style.backgroundColor="transparent";
+        i--;
         imgBox1.src=imgBox1Arr[i];
         imgBox2.src=imgBox2Arr[i];
         imgBox3.src=imgBox3Arr[i];
         nextBtn.style.opacity="1";
-        i--;
-       
     }
     else{
-       prevBtn.style.opacity="0.3";
-       nextBtn.style.opacity="1";
+        i=0;
+        dot[i].style.backgroundColor="var(--green)";
+        imgBox1.src=imgBox1Arr[i];
+        imgBox2.src=imgBox2Arr[i];
+        imgBox3.src=imgBox3Arr[i];
+        prevBtn.style.opacity="0.5";
     }
-
+    
     
 }
-let width= window.innerWidth;
-console.log(width)
+
+// end of project section script
+
+// let width= window.innerWidth;
+// console.log(width)
+
+
+let phoneNumber= document.getElementById('phoneNumber');
+phoneNumber.addEventListener('click',()=>{
+    window.alert("Call us on: +2348033754397 OR +2347036455204 ")
+})
 
 
 
